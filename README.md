618804a1ccc2183d319a97ae78586b26ff3b5f7e
Diogo Correia <me@diogotc.com>
2024-01-20 19:04:11 +0100

    chore: initialize project

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# lidl-to-grocy
+# Lidl to Grocy
+
 Import receipts from Lidl Plus to Grocy, right from your terminal!
+
+## Why?
+
+Why not?
+
+## Disclaimer
+
+This program makes unofficial use of the Lidl Plus API and might stop
+working at any time.
+It is, in no way, shape or form, associated with neither Lidl nor Grocy.
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            pkg-config
+            rustc
+            rustfmt
+            rust-analyzer
+            clippy
+          ];
+
+          shellHook = ''
+            export RUST_LOG=debug
+          '';
+        };
+      });
+}
